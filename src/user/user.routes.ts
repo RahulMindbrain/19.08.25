b27a591bc6811e11f31a6310ff2ac4e918b17335
Akshay-mindbrain <akshay.mindbrain@gmail.com>
2025-08-13 14:45:08 +0530

    final staging

--- src/user/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const UserRouter = express.Router()
-import { fetchUser,createUser } from './user.controller'
-
-UserRouter.get('/', fetchUser)
-UserRouter.post('/', createUser)
-
-export default UserRouter
